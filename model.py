a4f790efa24caa22a076495025252877ee823916
s-isaev <48261488+s-isaev@users.noreply.github.com>
2021-12-05 01:32:19 +0300

    full train

--- model.py
+++ model.py
@@ -36,28 +36,65 @@ class TransformerBlock(nn.Module):
 
 class LengthRegulator(nn.Module):
 
-    def __init__(self):
+    def __init__(self, hidden_size):
         super(LengthRegulator, self).__init__()
 
-    def forward(self, x, mels_alignations, mels_gt_num, token_nums):
-        mels_alignations = (mels_alignations.T/mels_alignations.sum(axis=1)*mels_gt_num).T
-        # print("Normalised")
-        # print(mels_alignations)
-        # print("Mels gt num:", mels_gt_num)
+        
+        self.conv1 = nn.Conv1d(
+            in_channels=hidden_size,
+            out_channels=hidden_size,
+            kernel_size=11,
+            padding='same'
+        )
+        self.ln1 = nn.LayerNorm(hidden_size)
+        self.relu1 = nn.ReLU()
+
+        self.conv2 = nn.Conv1d(
+            in_channels=hidden_size,
+            out_channels=hidden_size,
+            kernel_size=11,
+            padding='same'
+        )
+        self.ln2 = nn.LayerNorm(hidden_size)
+        self.relu2 = nn.ReLU()
+        self.linear = nn.Linear(hidden_size, 1)
+
+    def forward(self, x, token_nums, mels_alignations=None, mels_gt_num=None):
+        m = self.conv1(x.transpose(1,2)).transpose(1,2)
+        m = self.relu1(self.ln1(m))
+        m = self.conv2(m.transpose(1,2)).transpose(1,2)
+        m = self.relu2(self.ln2(m))
+        m = self.linear(m).squeeze()
+        for examle in range(x.shape[0]):
+            m[examle][token_nums[examle]:] = 0.0
+        #print(m)
+        mels_alignations_predicted = m
+
+        if mels_alignations is None:
+            mels_alignations = torch.exp(mels_alignations_predicted)
+            for examle in range(x.shape[0]):
+                mels_alignations[examle][token_nums[examle]:] = 0.0
+            mels_gt_num = mels_alignations.sum(axis=1).long()
+
+            print("mels alignations:", mels_alignations)
+            print("mels gt num:", mels_gt_num)
+
+
         x_new = torch.zeros((x.shape[0], int(mels_gt_num.max().item()), x.shape[2])).to('cuda:0')
         for examle in range(x.shape[0]):
             cur_mel = 0
             cur_tgt = 0
             for i in range(x.shape[1]):
-                cur_tgt += mels_alignations[examle][i]
-                cur_tgt = min(cur_tgt, mels_gt_num[examle])
+                # print(mels_alignations[examle][i])
+                cur_tgt = cur_tgt + mels_alignations[examle][i].item()
+                cur_tgt = min(cur_tgt, mels_gt_num[examle].item())
                 while cur_mel < cur_tgt:
+                    # print(mels_gt_num)
+                    # print(cur_mel, cur_tgt)
                     x_new[examle][cur_mel] = x[examle][i]
                     cur_mel += 1
-        # print("x_new:")
-        # print(x_new)
-        # print("x_new shape:", x_new.shape)
-        return x_new
+
+        return x_new, mels_alignations_predicted
 
 
 
@@ -76,7 +113,7 @@ class FastSpeech(nn.Module):
 
         self.encoder_fft = TransformerBlock(hidden_size=hidden_size)
 
-        self.length_regulator = LengthRegulator()
+        self.length_regulator = LengthRegulator(hidden_size=hidden_size)
 
         self.sec_position_enc = PositionalEncoding(
             emb_size=hidden_size, dropout=0.0
@@ -86,12 +123,13 @@ class FastSpeech(nn.Module):
 
         self.linear_layer = nn.Linear(hidden_size, n_mels)
 
-    def forward(self, tokens, mels_alignations, mels_gt_num, token_nums):
+    def forward(self, tokens, token_nums, mels_alignations=None, mels_gt_num=None):
         x = self.embedding(tokens)
         x = self.fir_position_enc(x)
         x = self.encoder_fft(x)
-        x = self.length_regulator(x, mels_alignations, mels_gt_num, token_nums)
+        x, mels_alignations_predicted = self.length_regulator(
+            x, token_nums, mels_alignations, mels_gt_num)
         x = self.sec_position_enc(x)
         x = self.decoder_fft(x)
         x = self.linear_layer(x)
-        return x
+        return x, mels_alignations_predicted
---
+++ traineval.py
@@ -0,0 +1,145 @@
+import torch
+from itertools import islice
+from torch._C import dtype
+from torch.nn.modules import loss
+from torch.utils.data import DataLoader
+
+from dataset import MelSpectrogramConfig, MelSpectrogram
+from dataset import LJSpeechDataset, LJSpeechCollator
+from dataset import GraphemeAligner
+
+from model import FastSpeech
+
+from vocoder import Vocoder
+import soundfile as sf
+import scipy.io.wavfile
+
+def train(epochs=50):
+    featurizer = MelSpectrogram(MelSpectrogramConfig())
+    dataset = LJSpeechDataset('.')
+    collator = LJSpeechCollator()
+    dataloader = DataLoader(dataset, batch_size=3, collate_fn=collator)
+
+    #dummy_batch = list(islice(dataloader, 1))[0]
+
+    device = torch.device('cuda:0')
+    aligner = GraphemeAligner().to(device)
+
+    # dummy_batch.durations = aligner(
+    #     dummy_batch.waveform.to(device), 
+    #     dummy_batch.waveforn_length, 
+    #     dummy_batch.transcript
+    #)
+
+    #print("Dummy batch:")
+    #print(dummy_batch)
+
+    model = FastSpeech(vocab_size=100, hidden_size=128, n_mels=80).to(device)
+    optimizer = torch.optim.Adam(model.parameters())
+    loss_mel = torch.nn.L1Loss().to(device)
+    loss_len = torch.nn.MSELoss().to(device) 
+
+    for batch in dataloader:
+        transcript_new = []
+        for text in batch.transcript:
+            text = text.replace("\"", "")
+            transcript_new.append(text)
+        batch.transcript = transcript_new
+
+        batch.durations = aligner(
+            batch.waveform.to(device), 
+            batch.waveforn_length, 
+            batch.transcript
+        )
+        tokens = batch.tokens.to(device)
+        mels_gt = featurizer(batch.waveform).cuda().transpose(1,2)
+
+        mels_gt_num = []
+        for i in range(batch.waveform.shape[0]):
+            wav = batch.waveform[i]
+            wav_len = batch.waveforn_length[i]
+            wav_len = wav_len.item()
+            mels_gt_num.append(featurizer(wav[:wav_len]).shape[1])
+
+        mels_alignations = batch.durations.to(device)
+        # print("Mels alignations shape before:", mels_alignations.shape)
+        mels_gt_num = torch.Tensor(mels_gt_num).to(device).long()
+        mels_alignations = (mels_alignations.T/mels_alignations.sum(axis=1)*mels_gt_num).T
+        token_nums = batch.token_lengths.to(device)
+        # print(mels_alignations[:, 110:])
+
+        optimizer.zero_grad()
+
+        # print("Tokens:", tokens.shape)
+        # print("Mels gt num:", mels_gt_num)
+        # print("Token nums:", token_nums)
+        # print("Mels alignations shape", mels_alignations.shape)
+            
+
+
+
+        mels, mels_alignations_predicted = model(tokens, token_nums, mels_alignations, mels_gt_num)
+        mels_alignations_log = mels_alignations
+        for examle in range(tokens.shape[0]):
+            mels_alignations_log[examle][token_nums[examle]:] = 1.0
+        mels_alignations_log = torch.log(mels_alignations_log)
+
+        if tokens.shape != mels_alignations.shape:
+            print(tokens.shpe)
+            print(mels_alignations.shape)
+            print(batch.transcript)
+            print(mels_alignations_predicted.shpape)
+        
+
+        loss_mel_n = loss_mel(mels, mels_gt)
+
+        loss_len_n = loss_len(mels_alignations_predicted, mels_alignations_log)
+        print("Mel:", loss_mel_n.item(), end=' ')
+        print("Len:", loss_len_n.item())
+
+        loss = loss_mel_n + loss_len_n
+        loss.backward()
+        optimizer.step()
+
+    return model, mels
+
+def eval(model, device):
+    featurizer = MelSpectrogram(MelSpectrogramConfig())
+    dataset = LJSpeechDataset('.')
+    collator = LJSpeechCollator()
+    dataloader = DataLoader(dataset, batch_size=3, collate_fn=collator)
+
+    dummy_batch = list(islice(dataloader, 1))[0]
+
+    aligner = GraphemeAligner().to(device)
+
+    dummy_batch.durations = aligner(
+        dummy_batch.waveform.to(device), 
+        dummy_batch.waveforn_length, 
+        dummy_batch.transcript
+    )
+
+    tokens = dummy_batch.tokens.to(device)
+    mels_gt = featurizer(dummy_batch.waveform).cuda().transpose(1,2)
+
+    mels_alignations = dummy_batch.durations.to(device)
+    token_nums = dummy_batch.token_lengths.to(device)
+
+    loss_mel = torch.nn.L1Loss().to(device)
+    loss_len = torch.nn.MSELoss().to(device) 
+
+
+    with torch.no_grad():
+        mels, mels_alignations_predicted = model(tokens, token_nums)
+
+        # loss_mel_n = loss_mel(mels, mels_gt)
+        loss_len_n = loss_len(mels_alignations_predicted, mels_alignations)
+        # print("Mel:", loss_mel_n.item(), end=' ')
+        print("Len:", loss_len_n.item())
+
+    vocoder = Vocoder().to(device).eval()
+    for i in range(mels.shape[0]):
+        mel = mels[i:i+1].transpose(1,2)
+        wav = vocoder.inference(mel).cpu().numpy()
+        print(wav[0])
+        scipy.io.wavfile.write(str(i) + '.wav',22050, wav[0])
